f7cd5108667f446d5903156a06b9d213873ad012
Nethwindu Gunathilaka <nethwindugunathilaka04@gmail.com>
2025-04-19 08:49:23 +0530

    final thymeleaf from lastweek

--- src/main/java/com/iit/schoolsys/StudentWebController.java
+++ src/main/java/com/iit/schoolsys/StudentWebController.java
@@ -1,6 +1,7 @@
 package com.iit.schoolsys;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/web")
 public class StudentWebController {
+
+    @Autowired
+    StudentService studentService;
 
     @RequestMapping("/home")
     public String home(Model model) {
